62b47cf7d65541eebc1f7b66a4a47f9ea068c956
Giulio Moro <giuliomoro@yahoo.it>
2017-06-11 19:43:45 +0100

    Copy xenomai-3 sources to the board

--- scripts/pre-chroot.sh
+++ scripts/pre-chroot.sh
@@ -1,21 +1,20 @@
 #!/bin/bash -e
 
-sudo mkdir -p $targetdir/opt/Bela
 
 sudo cp -v ${DIR}/kernel/*.deb $targetdir/root/
 sudo cp -v ${DIR}/kernel/kernel_version $targetdir/root/
-
 sudo cp -r ${DIR}/downloads/Bela $targetdir/root/
-
 sudo cp -R ${DIR}/downloads/clang+llvm-4.0.0-armv7a-linux-gnueabihf/* $targetdir/usr/local/
+
+sudo mkdir -p $targetdir/opt/Bela
 sudo cp -v ${DIR}/downloads/setup_7.x $targetdir/opt/Bela/
 sudo cp -r ${DIR}/downloads/prudebug $targetdir/opt/
 sudo cp -r ${DIR}/downloads/am335x_pru_package $targetdir/opt/
 sudo cp -r ${DIR}/downloads/bb.org-overlays $targetdir/opt/
 sudo cp -r ${DIR}/downloads/bb.org-dtc $targetdir/opt/
 sudo cp -r ${DIR}/downloads/dtb-rebuilder $targetdir/opt/
-sudo cp -r ${DIR}/downloads/boot-scripts $targetdir/opt/
-mv $targetdir/opt/boot-scripts $targetdir/opt/bb.org-scripts
+sudo cp -r ${DIR}/downloads/boot-scripts $targetdir/opt/bb.org-scripts
+sudo cp -r ${DIR}/downloads/xenomai-3 $targetdir/opt/
 
 # install xenomai to rootfs
 echo "~~~~ installing xenomai  ~~~~"
