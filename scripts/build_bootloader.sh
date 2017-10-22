adbd095336a69db81130a0ceddcbe2ab80d84dcc
Giulio Moro <giuliomoro@yahoo.it>
2017-10-22 15:21:17 +0100

    This should really get us through an interrupted bootloader build.

--- scripts/build_bootloader.sh
+++ scripts/build_bootloader.sh
@@ -1,14 +1,15 @@
-#!/bin/bash -e
+#!/bin/bash -x
 [ -z "$DIR" ] && DIR=$PWD
 [ -z "$CORES" ] && CORES=$(getconf _NPROCESSORS_ONLN)
 [ -z "$CC" ] && { echo "build_bootloader.sh: you should specify a cross compiler in \$CC" >& 2; exit 1; }
 
 echo "~~~~ compiling bootloader ~~~~"
 cd ${DIR}/downloads/u-boot
-git checkout v2017.03
-git branch -D tmp
-git checkout v2017.03 -b tmp
 git clean -f
+git checkout .
+git checkout v2017.03
+git branch -D tmp || true
+git checkout v2017.03 -b tmp
 cp -v ${DIR}/boot/bela_uboot_config.patch ./
 cp -v ${DIR}/boot/bela_uboot_bootcmd.patch ./
 git apply bela_uboot_config.patch
