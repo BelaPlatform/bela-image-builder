28eb59230b19e9945cbc430f2b6a46cb58f72b47
Giulio Moro <giuliomoro@yahoo.it>
2017-10-27 21:36:42 +0100

    Using LOOP instead of loop0

--- scripts/create_img.sh
+++ scripts/create_img.sh
@@ -19,10 +19,10 @@ LOOP=`echo $LOOP | sed "s/\/dev\///"`
 #sudo losetup /dev/$LOOP
 # -s makes sure the operation is applied before continuing
 sudo kpartx -s -av ${DIR}/bela.img
-sudo mkfs.vfat /dev/mapper/loop0p1
-sudo dosfslabel /dev/mapper/loop0p1 BELABOOT
-sudo mkfs.ext4 /dev/mapper/loop0p2
-sudo e2label /dev/mapper/loop0p2 BELAROOTFS
+sudo mkfs.vfat /dev/mapper/${LOOP}p1
+sudo dosfslabel /dev/mapper/${LOOP}p1 BELABOOT
+sudo mkfs.ext4 /dev/mapper/${LOOP}p2
+sudo e2label /dev/mapper/${LOOP}p2 BELAROOTFS
 
 mkdir -p /mnt/bela/boot
 mkdir -p /mnt/bela/root
