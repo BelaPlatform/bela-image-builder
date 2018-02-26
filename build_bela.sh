6793d39dbe91928bb4210102185affabdbefd540
Giulio Moro <giuliomoro@yahoo.it>
2018-02-26 16:56:14 +0000

    Added dependencies (required by newest kernel builder?)

--- build_bela.sh
+++ build_bela.sh
@@ -2,7 +2,7 @@
 
 # this script downloads, builds and compiles an image (including kernel, bootloader and rootfs) for Bela
 
-DEPENDENCIES="debootstrap qemu-arm-static autoreconf libtool arm-linux-gnueabihf-ranlib kpartx setuidgid wget"
+DEPENDENCIES="debootstrap qemu-arm-static autoreconf libtool arm-linux-gnueabihf-ranlib kpartx setuidgid wget bison flex"
 
 for a in $DEPENDENCIES; do
 	which $a > /dev/null ||\
