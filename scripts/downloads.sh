2919267fdd262757b8b15a30a1ec0c0cb9feb78d
Giulio Moro <giuliomoro@yahoo.it>
2018-10-04 11:58:11 +0100

    Removed supercollider packages (now provided by BelaPlatform/Bela)

--- scripts/downloads.sh
+++ scripts/downloads.sh
@@ -97,7 +97,3 @@ fi
 rm -rf deb
 mkdir -p deb && cd deb
 
-# get SuperCollider. TODO: track the most recent release
-wget "https://github.com/giuliomoro/supercollider/releases/download/3.9dev-Bela-build_20171011/supercollider-bela-xenomai-3_3.9dev-build_20171011_armhf.deb"
-wget "https://github.com/giuliomoro/supercollider/releases/download/3.9dev-Bela-build_20171011/sc3-plugins_20180226-1_armhf.deb"
-
